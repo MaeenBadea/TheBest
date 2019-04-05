3383163aa95982101cd97e9a3cc6141e84c61e5a
MaeenBadea <34209166+MaeenBadea@users.noreply.github.com>
2019-04-05 19:30:53 +0400

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,14 +8,7 @@ export const FIREBASE_MESSAGING_SENDER_ID = <YOUR_FIREBASE_MESSAGING_SENDER_ID>;
 export const FACEBOOK_APP_ID = <YOUR_FACEBOOK_APP_ID>;
 */
 
-export const FIREBASE_API_KEY = "AIzaSyB-iLfyodW-fqsmT_3HWt627-4aRr7ZC_w";
-export const FIREBASE_AUTH_DOMAIN = "el-mejor-68faa.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://el-mejor-68faa.firebaseio.com/";
-export const FIREBASE_PROJECT_ID = "el-mejor-68faa";
-export const FIREBASE_STORAGE_BUCKET = "gs://el-mejor-68faa.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "88585380095";
 
-export const FACEBOOK_APP_ID = "247557055954440";
 
 export const APP = {
     TAB: 'APP.TAB',
